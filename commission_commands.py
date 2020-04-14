ba68682646f454e588b8f0544f5c6264ce968e13
DrFacepalm <lucaslin111@gmail.com>
2020-04-14 23:34:46 +1000

    updates to commission commands

--- commission_commands.py
+++ commission_commands.py
@@ -9,20 +9,22 @@ import discord
 # COMISSIONED COMMAND ##############################
 # COMMISSIONED BY Zachaccino:430382953659367425 ###
 # PURRRGE: purges based on amount of Rs in command #
-# without argument, purges X indiscrimantely, with #
+# without argument, purges X jayden, with ##########
 # singular mention, purges X if specified user #####
+# 'all; case insensitive purges indiscrimiately ####
 ####################################################
 async def _process_PURRRGE(message):
     
     # Check command
     if (message.content.startswith('!PUR')):
-        command = message.content.split(" ")[0]
+        msg_list = message.content.split(" ")
+        command = msg_list[0]
 
         # Check command
         if (command.endswith('GE') and command[3:-2] == "R"*len(command[3:-2])):
 
             # Set limit
-            limit = min([len(command[3:-2]), 5]) * 10
+            limit = min([len(command[3:-2]), 20])
 
             # Check if zac                                                      or me
             if message.author.id == 430382953659367425 or message.author.id == 248410042498285569:
@@ -33,9 +35,16 @@ async def _process_PURRRGE(message):
                     # Set target's name
                     target_name = message.mentions[0].nick if message.mentions[0].nick else message.mentions[0].name
                     await message.channel.send(f":knife: It would seem that Zac has had it with you {target_name}")
+                elif (len(msg_list) > 1):
+                    if msg_list[1].lower() == "all":
+                        deleted = await message.channel.purge(limit=limit, check=(lambda m: True))
+                        print(f"Deleted {len(deleted)}")
+                        await message.channel.send(f":knife: It would seem that Zac has had it with this conversation")
                 else:
-                    deleted = await message.channel.purge(limit=limit, check=(lambda m: True))
+                    # does jayden's bot
+                    target = 698107749166219284
+                    deleted = await message.channel.purge(limit=limit, check=(lambda m: m.author.discriminator == target))
                     print(f"Deleted {len(deleted)}")
-                    await message.channel.send(f":knife: It would seem that Zac has had it with this conversation.")
+                    await message.channel.send(f":knife: It would seem that Zac has had it with you Jayden.")
             else:
-                await message.channel.send("Wait a minute, you aren't Zac. Go be Zac and try again")   
+                await message.channel.send("Wait a minute, you aren't Zac. Go be Zac and try again.\nThis incident has been reported to Administration.")   
