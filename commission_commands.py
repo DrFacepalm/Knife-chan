54556a0fc4a25a1989f6d3bcb3523e66239fb748
DrFacepalm <lucaslin111@gmail.com>
2020-04-15 01:44:38 +1000

    minor bug fix

--- commission_commands.py
+++ commission_commands.py
@@ -30,10 +30,11 @@ async def _process_PURRRGE(message):
             if message.author.id == 430382953659367425 or message.author.id == 248410042498285569:
                 if (len(message.mentions) > 0):
                     target = message.mentions[0].discriminator
-                    deleted = await message.channel.purge(limit=limit*3, check=(lambda m : m.author.discriminator == target))
+                    limit *= 3
+                    deleted = await message.channel.purge(limit=limit, check=(lambda m : m.author.discriminator == target))
                     # Set target's name
                     target_name = message.mentions[0].nick if message.mentions[0].nick else message.mentions[0].name
-                    await message.channel.send(f":knife: It would seem that Zac has had it with you {target_name}.\n*Deleted {len(deleted)} out of the last {limit*3} messages*")
+                    await message.channel.send(f":knife: It would seem that Zac has had it with you {target_name}.\n*Deleted {len(deleted)} out of the last {limit} messages*")
                 elif (len(msg_list) > 1):
                     if msg_list[1].lower() == "all":
                         deleted = await message.channel.purge(limit=limit, check=(lambda m: True))
@@ -42,7 +43,7 @@ async def _process_PURRRGE(message):
                     # does jayden's bot
                     target = 698107749166219284
                     limit = min(limit * 10, 100)
-                    deleted = await message.channel.purge(limit=limit*3, check=(lambda m : m.author.id == target))
+                    deleted = await message.channel.purge(limit=limit, check=(lambda m : m.author.id == target))
                     await message.channel.send(f":knife: It would seem that Zac has had it with ImagusBot.\n*Deleted {len(deleted)} out of the last {limit} messages*")
             else:
                 await message.channel.send("Wait a minute, you aren't Zac. Go be Zac and try again.\nThis incident has been reported to Administration.")   
